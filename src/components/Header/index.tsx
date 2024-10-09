60fa6a53d4f1c2845416392d9e948d523350053b
00043963 <00043963@randon.com.br>
2024-10-09 16:38:06 -0300

    feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,29 @@
-export default function HeaderChat() {
-    return (
-        <header className="bg-header-gradient w-screen h-24 flex items-center">
-            <div className="flex items-center w-full px-4">
-                <div className="relative w-24 h-12">
-                    <img
-                        src=""
-                        alt="Logo"
-                        className="object-contain w-full h-full"
-                    />
-                </div>
-            </div>
-        </header>
-    );
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export default function Header() {
+  return (
+    <header className="bg-blue-100 w-full h-24 flex items-center justify-between px-8 border border-blue-300">
+      <div className="flex items-center space-x-20">
+        <div className="text-3xl font-bold text-blue-900">{'<db>'}</div>
+        <nav className="flex space-x-12 text-blue-900 font-semibold">
+          <a href="#sobre">Sobre</a>
+          <a href="#curriculo">Currículo</a>
+          <a href="#projetos">Projetos</a>
+        </nav>
+      </div>
+
+      <div className="flex space-x-8 text-blue-900 mr-10">
+        <div>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={24} />
+            </a>
+        </div>
+        <div>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={24} />
+            </a>
+        </div>
+      </div>
+    </header>
+  );
 }
