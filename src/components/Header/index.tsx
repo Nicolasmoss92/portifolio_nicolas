6a8d016a76f9f281325da6bc1d01e262af67fc80
00043963 <00043963@randon.com.br>
2024-10-09 11:27:27 -0300

    feat: initial structure header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+export default function HeaderChat() {
+    return (
+        <header className="bg-header-gradient w-screen h-24 flex items-center">
+            <div className="flex items-center w-full px-4">
+                <div className="relative w-24 h-12">
+                    <img
+                        src=""
+                        alt="Logo"
+                        className="object-contain w-full h-full"
+                    />
+                </div>
+            </div>
+        </header>
+    );
+}
