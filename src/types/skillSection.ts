4a0462f505bd7aa116435a83ec5d2f76d23ffc33
00043963 <00043963@randon.com.br>
2024-10-14 00:01:03 -0300

    feat: habilits

--- src/types/skillSection.ts
+++ src/types/skillSection.ts
@@ -1,4 +1,4 @@
-interface Skill {
+export interface Skill {
     name: string;
     filled: number;
     total: number;
@@ -7,3 +7,21 @@ interface Skill {
 export interface SkillsSectionProps {
     skills: Skill[];
 }
+
+
+const skills = [
+    { name: "NODE", filled: 4, total: 5 },
+    { name: "PYTHON", filled: 3, total: 5 },
+    { name: "JAVASCRIPT", filled: 5, total: 5 },
+    { name: "REACT", filled: 4, total: 5 },
+    { name: "NESTJS", filled: 3, total: 5 },
+    { name: "NEXTJS", filled: 4, total: 5 },
+    { name: "TYPESCRIPT", filled: 4, total: 5 },
+    { name: "TAILWIND", filled: 4, total: 5 },
+    { name: "DOCKER", filled: 3, total: 5 },
+    { name: "MYSQL", filled: 3, total: 5 },
+    { name: "POSTGRES", filled: 3, total: 5 },
+];
+
+export default skills;
+  
