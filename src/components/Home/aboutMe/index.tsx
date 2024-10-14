8481a62f62dff0fb9cc8cf886114a7678cd60657
00043963 <00043963@randon.com.br>
2024-10-14 10:55:54 -0300

    feat: test

--- src/components/Home/aboutMe/index.tsx
+++ src/components/Home/aboutMe/index.tsx
@@ -7,22 +7,22 @@ export default function AboutMeSection() {
         <h2 className="supermolot text-3xl font-bold mb-4">
           Sobre mim
         </h2>
-        <p className="mb-4">
+        <p className="supermolot mb-4">
           Iniciei minha carreira acadêmica cursando Engenharia de Energia na UFRGS.
           Após dois anos e meio de curso, percebi que meu verdadeiro interesse estava na área de tecnologia.
           Foi então que decidi trocar de curso e iniciei a graduação em Análise e Desenvolvimento de Sistemas na UCS,
           onde me formei no final de 2023.
         </p>
-        <p className="mb-4">
+        <p className="supermolot mb-4">
           Iniciei minha trajetória na área de tecnologia como técnico de software.
           Após adquirir uma experiência inicial, decidi migrar para a área de desenvolvimento, onde atuo até hoje.
         </p>
-        <p className="mb-4">
+        <p className="supermolot mb-4">
           Ao longo da minha carreira profissional, adquiri habilidades em trabalho em equipe,
           desenvolvimento backend e um conhecimento inicial de frontend. Gosto de ajudar meus colegas e encaro
           os projetos com responsabilidade, sempre buscando entregar um bom resultado.
         </p>
-        <p className="mb-4">
+        <p className="supermolot mb-4">
           Atualmente, estou em busca de um novo desafio na área de desenvolvimento fullstack.
           Estou ansioso para aplicar minhas habilidades em backend e frontend em projetos 
           que me permitam crescer e contribuir para soluções inovadoras.
